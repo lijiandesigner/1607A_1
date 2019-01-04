1234c5f8c4a1809c23f37c6ab01329ac8a8e0230
2941451132@qq.com <2941451132@qq.com>
2019-01-04 11:44:18 +0800

    更改

--- ERP_API/ERP_Bll/AttendanceInfoBll.cs
+++ ERP_API/ERP_Bll/AttendanceInfoBll.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ERP_Model;
 using ERP_Dal;
 using System.Data;
+using Models
 
 namespace ERP_Bll
 {
     public class AttendanceInfoBll
     {
 
-        public int MyProperty1 { get; set; }
     }
 }
