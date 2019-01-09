63db7992ec0b2179087f45430c9a0230a6e1d0af
bitType <2386690386@qq.com>
2019-01-09 21:02:33 +0800

    12

---
+++ ERP_API/Models/WorkState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class WorkState
+    {
+        public int PID { get; set; }//主键ID
+        public string ENo { get; set; }//员工编号
+        public string EName { get; set; }//姓名
+        public string PeBeginWork { get; set; }//入职时间
+        public string PeEndwork { get; set; }//离职时间
+        public bool Pstatic { get; set; }//状态
+    }
+}
