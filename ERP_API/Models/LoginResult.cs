8625f4bdf85b0080b0d0465372e21177995fd07f
bitType <2386690386@qq.com>
2019-01-10 11:28:52 +0800

    11

--- ERP_API/Models/LoginResult.cs
+++ ERP_API/Models/LoginResult.cs
@@ -7,10 +7,11 @@ namespace Models
 {
     public class LoginResult
     {
-        public Boolean Result { get; set; }
-        public int EID { get; set; }
-        public string PoName { get; set; }
-        public string EName { get; set; }
-        public string Permissins { get; set; }
+        public Boolean Result { get; set; }//登录是否成功
+        public int EID { get; set; }//员工id
+        public string PoName { get; set; }//职位名称
+        public string ENo { get; set; }//员工编号
+        public string EName { get; set; }//员工姓名
+        public string Permissins { get; set; }//职位权限
     }
 }
