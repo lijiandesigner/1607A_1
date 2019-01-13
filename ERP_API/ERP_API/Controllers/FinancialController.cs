33fd011f867597e03009111c74a6e98f96485697
bitType <2386690386@qq.com>
2019-01-13 20:05:10 +0800

    456

--- ERP_API/ERP_API/Controllers/FinancialController.cs
+++ ERP_API/ERP_API/Controllers/FinancialController.cs
@@ -4,10 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Models;
+using ERP_Bll;
 
 namespace ERP_API.Controllers
 {
+    /// <summary>
+    /// 财务
+    /// </summary>
     public class FinancialController : ApiController
     {
+        /// <summary>
+        /// 根据条件查询员工工资
+        /// </summary>
+        /// <returns></returns>
+        public static List<GZ> GetGZs()
+        {
+            return WagesInfoBll.GetGZs();
+        }
+        /// <summary>
+        /// 生成本月工资记录
+        /// </summary>
+        /// <returns></returns>
+        public static int CreateGZ(string Date)
+        {
+            return WagesInfoBll.CreateGZ(Date);
+        }
     }
 }
--- ERP_API/ERP_Bll/WagesInfoBll.cs
+++ ERP_API/ERP_Bll/WagesInfoBll.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERP_Dal;
+using Models;
 
 namespace ERP_Bll
 {
     public class WagesInfoBll
     {
+        /// <summary>
+        /// 根据条件查询员工工资
+        /// </summary>
+        /// <returns></returns>
+        public static List<GZ> GetGZs()
+        {
+            return WagesInfoDal.GetGZs();
+        }
+        /// <summary>
+        /// 生成本月工资记录
+        /// </summary>
+        /// <returns></returns>
+        public static int CreateGZ(string Date)
+        {
+            return WagesInfoDal.CreateGZ(Date);
+        }
     }
 }
