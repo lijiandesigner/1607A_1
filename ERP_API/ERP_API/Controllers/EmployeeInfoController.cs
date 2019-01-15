7a5419ae7789cf69c04a161df22a0de9920e46b0
bitType <2386690386@qq.com>
2019-01-15 09:33:36 +0800

    111

--- ERP_API/ERP_API/Controllers/EmployeeInfoController.cs
+++ ERP_API/ERP_API/Controllers/EmployeeInfoController.cs
@@ -18,13 +18,10 @@ namespace ERP_API.Controllers
         /// <summary>
         /// 根据条件获取员工信息
         /// </summary>
-        /// <param name="ENo">员工编号</param>
-        /// <param name="EName">员工姓名</param>
-        /// <param name="Pstatic">工作状态</param>
         /// <returns></returns>
-        public List<EmployeeInfos> Get(bool Pstatic, string ENo = null, string EName = null)
+        public List<EmployeeInfos> Get()
         {
-            return EmployeeInfoBll.GetEmployeeInfos(ENo, EName, Pstatic);
+            return EmployeeInfoBll.GetEmployeeInfos();
         }
         /// <summary>
         /// 根据员工ID获取单个对象
--- ERP_MVC/ERP_MVC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ERP_MVC.MvcApplication" Language="C#" %>
